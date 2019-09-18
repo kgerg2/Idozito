63addbb685bd1727c8292be54331697df606bab0
kgerg2 <46970565+kgerg2@users.noreply.github.com>
2019-09-18 18:33:14 +0200

    Teszt - Feladatok létrehozása és listázása

--- bekapcs.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import json
-import ast
-
-a = subprocess.run("SCHTASKS /Query /NH /FO CSV".split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode("unicode_escape", errors="replace")
-a = "[[" + a[:-2].replace("\r\n", "], [").replace('"', '\"') + "]]"
-a = ast.literal_eval(a)
-a = list(filter(lambda x: x[0][:6] == "\\kgerg", a))
-print (a)
