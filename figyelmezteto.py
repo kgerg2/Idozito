3d52ef99413a96560e132b517f362f8334bc6409
kgerg2 <46970565+kgerg2@users.noreply.github.com>
2020-01-04 09:43:07 +0100

    Rövidtávú halasztásnál a program a háttérben fut

--- figyelmezteto.py
+++ figyelmezteto.py
@@ -10,35 +10,11 @@ Kattints ide, hogy alvó állapotba rakd!"""
 ora_ikon = "C:\\Users\\kgerg\\Documents\\GitHub\\Idozito\\clock-icon.ico"
 nyil_ikon = "C:\\Users\\kgerg\\Documents\\GitHub\\Idozito\\nyíl-ikon.png"
 hossz = 5
-
-
-ablak = tkinter.Tk()
-ablak.title(cim)
-ablak.iconbitmap(ora_ikon)
-ablak.overrideredirect(True)
-ablak.wm_attributes("-topmost", True)
-ablak.geometry("380x100+880+540")
-ablak.wm_attributes("-alpha", 0.95)
-ablak.wm_attributes("-toolwindow", True)
-
-
-cimsor = tkinter.Frame(ablak)
-cimsor.columnconfigure(index=0, minsize=340)
-
-cim = ttk.Label(cimsor, text=cim, font=(None, 10, 'bold'))
-cim.grid(row=0, column=0, padx=10, pady=5, sticky="w")
-
-nyil_kep = tkinter.PhotoImage(file=nyil_ikon)
-nyil_gomb = tkinter.Button(cimsor, image=nyil_kep, borderwidth=0, command=ablak.quit)
-nyil_gomb.grid(row=0, column=1, sticky="e")
-
-cimsor.pack(anchor="n", fill="x")
-
-
 halaszt_ertekek = ("1 perc", "2 perc", "5 perc", "10 perc", "30 perc", "1 óra", "2 óra")
 
+
 def keszenlet():
-    subprocess.run("C:\\Users\\kgerg\\Documents\\Programok\\nircmd.exe standby")
+    subprocess.run("C:\\Users\\kgerg\\Documents\\Programok\\nircmd.exe standby", check=False)
     exit()
 
 def ido_ertelmezo(szoveg):
@@ -71,35 +47,57 @@ def ido_ertelmezo(szoveg):
 
     raise ValueError(f"{szoveg} nem alakítható idővé.")
 
-def halaszt(_):
+def halaszt(*_):
     ido = halaszt_lista.get().lower()
     try:
-        ido = ido_ertelmezo(ido)
-        ablak.quit()
-        print(ido)
+        ido = ido_ertelmezo(ido)*60 or 30
+    except ValueError:
+        ido = 30
+    ablak.destroy()
+    if ido <= 120:
+        time.sleep(ido)
+    else:
         utemezett = [nev for nev, _ in listaz()]
-        print(utemezett)
         if "figyelmezteto" in utemezett:
             torol("figyelmezteto")
-        print("törölve")
-        ido = tuple(time.localtime(time.time() + ido*60))[3:5]
-        print(letrehoz("figyelmezteto", "figyelmezteto", "{:02}:{:02}".format(*ido)))
-    except ValueError:
-        pass
+        ido = tuple(time.localtime(time.time() + ido))[3:5]
+        letrehoz("figyelmezteto", "figyelmezteto", "{:02}:{:02}".format(*ido))
+        exit()
 
-keszenlet_gomb = ttk.Button(ablak, text="Készenlét", command=keszenlet)
-keszenlet_gomb.pack(side=tkinter.LEFT, padx=10)
-
-halaszt_lista = ttk.Combobox(ablak, values=tuple(halaszt_ertekek))
-halaszt_lista.pack(side=tkinter.RIGHT, padx=10)
-halaszt_lista.bind("<Key-Return>", halaszt)
-halaszt_lista.bind("<<ComboboxSelected>>", halaszt)
-
-halaszt_szoveg = ttk.Label(ablak, text="Halasztás:")
-halaszt_szoveg.pack(side=tkinter.RIGHT)
+while True:
+    ablak = tkinter.Tk()
+    ablak.overrideredirect(True)
+    ablak.wm_attributes("-topmost", True)
+    ablak.geometry("380x100+880+540")
+    ablak.wm_attributes("-alpha", 0.95)
 
 
-ablak.after(15000, ablak.quit)
-ablak.bell()
+    cimsor = tkinter.Frame(ablak)
+    cimsor.columnconfigure(index=0, minsize=340)
 
-ablak.mainloop()
+    cim_szoveg = ttk.Label(cimsor, text=cim, font=(None, 10, 'bold'))
+    cim_szoveg.grid(row=0, column=0, padx=10, pady=5, sticky="w")
+
+    nyil_kep = tkinter.PhotoImage(file=nyil_ikon)
+    nyil_gomb = tkinter.Button(cimsor, image=nyil_kep, borderwidth=0, command=halaszt)
+    nyil_gomb.grid(row=0, column=1, sticky="e")
+
+    cimsor.pack(anchor="n", fill="x")
+
+
+    keszenlet_gomb = ttk.Button(ablak, text="Készenlét", command=keszenlet)
+    keszenlet_gomb.pack(side=tkinter.LEFT, padx=10)
+
+    halaszt_lista = ttk.Combobox(ablak, values=tuple(halaszt_ertekek))
+    halaszt_lista.pack(side=tkinter.RIGHT, padx=10)
+    halaszt_lista.bind("<Key-Return>", halaszt)
+    halaszt_lista.bind("<<ComboboxSelected>>", halaszt)
+
+    halaszt_szoveg = ttk.Label(ablak, text="Halasztás:")
+    halaszt_szoveg.pack(side=tkinter.RIGHT)
+
+
+    ablak.after(10000, halaszt)
+    ablak.bell()
+
+    ablak.mainloop()
